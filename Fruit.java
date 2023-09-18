5964b03855ac98466d6ecc29c923d031c85611c4
maxim_stoma <maksim.stoma@mail.ru>
2023-09-18 03:38:19 +0300

    created base class Fruit and extended classes (Apple & Orange)

---
+++ Fruit.java
@@ -0,0 +1,11 @@
+public abstract class Fruit {
+    private final float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
