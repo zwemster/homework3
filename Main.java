d6e64a926ddc72bf23236da26d90582c7b5513d9
maxim_stoma <maksim.stoma@mail.ru>
2023-09-18 03:50:41 +0300

    updated the Main class (added 3 boxes with fruits, used all methods to check if everything is working)

--- Main.java
+++ Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /**
+         * Создание первой коробки с яблоками
+         */
+        Box<Apple> appleBox1 = new Box<>();
+        appleBox1.addFruits(new Apple());
+        appleBox1.addFruits(new Apple());
+
+        /**
+         * Создание первой коробки с апельсинами
+         */
+
+        Box<Orange> orangeBox1 = new Box<>();
+        orangeBox1.addFruits(new Orange());
+        orangeBox1.addFruits(new Orange());
+
+        /**
+         * Создание второй коробки с яблоками
+         */
+
+        Box<Apple> appleBox2 = new Box<>();
+        appleBox2.addFruits(new Apple());
+        appleBox2.addFruits(new Apple());
+
+        /**
+         * Производится сравнение коробок
+         */
+
+        System.out.println("Вес коробки с яблоками: " + appleBox1.getWeight());
+        System.out.println("Вес коробки с апельсинами: " + orangeBox1.getWeight());
+        System.out.println("Коробки равны по весу: " + appleBox1.compare(orangeBox1));
+
+        /**
+         * Производится пересыпание фруктов в другой ящик
+         */
+
+        appleBox1.transferToAnotherBox(appleBox2);
+
+        /**
+         * Производится повторное сравнение коробок
+         */
+
+        System.out.println("Вес коробки с яблоками: " + appleBox1.getWeight());
+        System.out.println("Вес второй коробки с яблоками: " + appleBox2.getWeight());
+        System.out.println("Коробки равны по весу: " + appleBox1.compare(appleBox2));
     }
 }
